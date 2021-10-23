9db2762e36327456d4970f2be6ac057638a6c83f
Kevin <kkivikas@tktk.com>
2021-10-23 15:39:13 +0300

    RandomFilesChuckNorris

---
+++ ConsoleApp1/ChuckNorris/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ChuckNorris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string randomChuck;
+            randomChuck = GetRandomFromJoke("chuck.txt");
+            Console.WriteLine(randomChuck);
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+            return randomIndex;
+        }
+
+        private static string GetRandomFromJoke(string fileName)
+        {
+            string filePath = $@"C:\Users\...\samples\{fileName}";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            string randomElement = dataFromFile[GenerateRandomIndex(dataFromFile)];
+
+            return randomElement;
+        }
+    }
+}
